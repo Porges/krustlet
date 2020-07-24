8c6e2e04ecfeb12f432d7f684d08a878cacb79c5
George Pollard <gpollard@microsoft.com>
2020-07-24 15:47:58 +1200

    Rename HandleFactory & Stopper
    Give them some more functionality-specific names.

--- crates/kubelet/src/container/handle.rs
+++ crates/kubelet/src/container/handle.rs
@@ -5,29 +5,29 @@ use tokio::sync::watch::Receiver;
 
 use crate::container::{ContainerMap, Status};
 use crate::handle::StopHandler;
-use crate::log::{stream, HandleFactory, Sender};
+use crate::log::{stream, LogReaderFactory, Sender};
 
 /// Represents a handle to a running "container" (whatever that might be). This
 /// can be used on its own, however, it is generally better to use it as a part
 /// of a [`pod::Handle`], which manages a group of containers in a Kubernetes
 /// Pod
-pub struct Handle<H, F> {
-    handle: H,
-    handle_factory: F,
+pub struct Handle<S, F> {
+    stopper: S,
+    log_reader_factory: F,
     status_channel: Receiver<Status>,
 }
 
-impl<H: StopHandler, F> Handle<H, F> {
+impl<S: StopHandler, F> Handle<S, F> {
     /// Create a new runtime with the given handle for stopping the runtime,
     /// a reader for log output, and a status channel.
     ///
     /// The status channel is a [Tokio watch `Receiver`][Receiver]. The sender part
     /// of the channel should be given to the running process and the receiver half
     /// passed to this constructor to be used for reporting current status
-    pub fn new(handle: H, handle_factory: F, status_channel: Receiver<Status>) -> Self {
+    pub fn new(stopper: S, log_reader_factory: F, status_channel: Receiver<Status>) -> Self {
         Self {
-            handle,
-            handle_factory,
+            stopper,
+            log_reader_factory,
             status_channel,
         }
     }
@@ -35,19 +35,19 @@ impl<H: StopHandler, F> Handle<H, F> {
     /// Signal the running instance to stop. Use [`Handle::wait`] to wait for the process to
     /// exit. This uses the underlying [`StopHandler`] implementation passed to the constructor
     pub async fn stop(&mut self) -> anyhow::Result<()> {
-        self.handle.stop().await
+        self.stopper.stop().await
     }
 
     /// Streams output from the running process into the given sender.
     /// Optionally tails the output and/or continues to watch the file and stream changes.
-    pub(crate) async fn output<R>(&mut self, sender: Sender) -> anyhow::Result<()>
+    pub(crate) async fn output(&mut self, sender: Sender) -> anyhow::Result<()>
     where
-        R: AsyncRead + AsyncSeek + Unpin + Send + 'static,
-        F: HandleFactory<R>,
+        F: LogReaderFactory,
+        F::Reader: AsyncRead + AsyncSeek + Unpin + Send + 'static,
     {
-        let mut handle = self.handle_factory.new_handle();
-        handle.seek(SeekFrom::Start(0)).await?;
-        tokio::spawn(stream(handle, sender));
+        let mut reader = self.log_reader_factory.new_reader();
+        reader.seek(SeekFrom::Start(0)).await?;
+        tokio::spawn(stream(reader, sender));
         Ok(())
     }
 
@@ -61,7 +61,7 @@ impl<H: StopHandler, F> Handle<H, F> {
     /// [`Handle::stop`] should be called first. This uses the underlying
     /// [`StopHandler`] implementation passed to the constructor
     pub(crate) async fn wait(&mut self) -> anyhow::Result<()> {
-        self.handle.wait().await
+        self.stopper.wait().await
     }
 }
 
